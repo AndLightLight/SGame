7b3172a929d5816723e421d438d9128f3aabd5b0
hellengoodd <327762932@qq.com>
2016-04-27 22:27:42 +0800

    no message

--- SGameProject/assets/script/MapHandle.js
+++ SGameProject/assets/script/MapHandle.js
@@ -12,16 +12,80 @@ cc.Class({
         // },
         // ...
         pre: [cc.Prefab],
+        map: [],
+        samenum: 3,
+        width: 15,
+        height: 15,
     },
 
     // use this for initialization
     onLoad: function () {
+
+    },
+    
+    checkBom: function (idx) {
+        var box = this.map[idx];
+        if (box) {
+            var ctnode = box.getComponent(require("boxnode"));
+            if (ctnode) {
+                var ctype = ctnode.type;
+                var backtype = [samenum - 1];
+                for (var index = 0; index < samenum - 1; index++) {
+                    backtype[index] = this.map[idx - index - 1]?
+                    (this.map[idx - index - 1].getComponent(require("boxnode"))?
+                    (this.map[idx - index - 1].getComponent(require("boxnode")).type):0):0;
+                    if (backtype[index] != ctype) {
+                        return false;
+                    }
+                }
+                for (var index = 0; index < samenum - 1; index++) {
+                    backtype[index] = this.map[idx + index + 1]?
+                    (this.map[idx + index + 1].getComponent(require("boxnode"))?
+                    (this.map[idx + index + 1].getComponent(require("boxnode")).type):0):0;
+                    if (backtype[index] != ctype) {
+                        return false;
+                    }
+                }
+                for (var index = 0; index < samenum - 1; index++) {
+                    backtype[index] = this.map[idx - this.width *(index + 1)]?
+                    (this.map[idx - this.width *(index + 1)].getComponent(require("boxnode"))?
+                    (this.map[idx - this.width *(index + 1)].getComponent(require("boxnode")).type):0):0;
+                    if (backtype[index] != ctype) {
+                        return false;
+                    }
+                }
+                for (var index = 0; index < samenum - 1; index++) {
+                    backtype[index] = this.map[idx + this.width *(index + 1)]?
+                    (this.map[idx + this.width *(index + 1)].getComponent(require("boxnode"))?
+                    (this.map[idx + this.width *(index + 1)].getComponent(require("boxnode")).type):0):0;
+                    if (backtype[index] != ctype) {
+                        return false;
+                    }
+                }
+                
+                return true;
+            }
+        }
+        
+        return false;
+    },
+    
+    createnodecall: function () {
+    },
+    
+    start: function () {
         var map = this.getComponent(cc.TiledLayer);
         var tiles = map.getTiles();
-        for (var tile in tiles) {
-            
+        for (var i in tiles) {
+            var bnot = tiles[i];
+            if (bnot == 0) {
+                var num = this.pre.length;
+                var r = Math.ceil(Math.random()*(num-1)*num);
+                var pre = this.pre[r-1];
+                pre.createNode(this.createnodecall);
+            }
         }
-    },
+    }
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
